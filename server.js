084b71c61404bbc1f155a9a2dd20cd19b50b59af
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-02 20:10:47 +0200

    Started implementing login route

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require("express");
-
 const app = express();
-
 const { createToken } = require("./jsonFile");
 
-console.log(createToken({ name: "Victor" }))
+const PORT = 3000;
+
+app.use(express.json());
+
+const user = {
+  firstName: "Victor",
+  lastName: "Mugisha",
+  username: "VictorMugisha",
+  password: "password",
+};
+
+// Mock a login route
+app.post("/login", (req, res) => {
+  const { firstName, lastName, username, password } = req.body;
+  if (username === user.username && password === user.password) {
+  } else {
+    res.status(400).json({ error: "Invalid username or password" });
+  }
+});
