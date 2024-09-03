f2b131c3793ad2837a3bfea539c4220c181ce9e1
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-03 08:05:59 +0200

    Now the login route is effectively working

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const { createToken } = require("./jsonFile");
 const { verify } = require("jsonwebtoken");
-require('dotenv').config()
+require("dotenv").config();
 
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const PORT = 3000;
 
 app.use(express.json());
@@ -19,35 +19,39 @@ const user = {
 
 // JWT token validation middleware
 const validateToken = (req, res, next) => {
-    const authHeader = req.headers.authentication;
-    if (!authHeader) {
-        res.status(401).json({
-            success: false,
-            message: 'Token is not provided'
-        });
-    } else {
-        const token = authHeader.split(' ')[1]; // Bearer <token>
+  const authHeader = req.headers.authentication;
+  if (!authHeader) {
+    res.status(401).json({
+      success: false,
+      message: "Token is not provided",
+    });
+  } else {
+    const token = authHeader.split(" ")[1]; // Bearer <token>
 
-        verify(token, JWT_SECRET_KEY, (err, decoded) => {
-            if (err) {
-                return res.status(403).json({
-                    success: false,
-                    message: 'Invalid token'
-                });
-            } else {
-                req.user = decoded;
-                next()
-            }
-        })
-    }
-}
+    verify(token, JWT_SECRET_KEY, (err, decoded) => {
+      if (err) {
+        return res.status(403).json({
+          success: false,
+          message: "Invalid token",
+        });
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  }
+};
 
 // Mock a login route
 app.post("/login", (req, res) => {
   const { username, password } = req.body;
   if (username === user.username && password === user.password) {
     const token = createToken(user);
-    res.json({ success: true, message: 'Authentication successful!', token: token });
+    res.json({
+      success: true,
+      message: "Authentication successful!",
+      token: token,
+    });
   } else {
     res
       .status(401)
@@ -57,8 +61,8 @@ app.post("/login", (req, res) => {
 
 // The protected route
 app.get("/protected", validateToken, (req, res) => {
-  res.json({ success: true, message: 'Protected route!', user: req.user });
-})
+  res.json({ success: true, message: "Protected route!", user: req.user });
+});
 
 // now the endpoint for login is http://localhost:3000/login
 
