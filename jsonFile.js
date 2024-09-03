fa2380a77d9e8fcd0315bbb5ec0fa1610f263a55
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-03 07:24:49 +0200

    Added validateToken middleware

--- jsonFile.js
+++ jsonFile.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const { sign, verify } = require("jsonwebtoken");
 
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
+
 const generateToken = (payload) => {
-    const secretKey = "someSecretString";
+    const secretKey = JWT_SECRET_KEY;
     const options = { expiresIn: "1d" };
     const token = sign(payload, secretKey, options);
     
--- server.js
+++ server.js
@@ -1,23 +1,60 @@
 const express = require("express");
 const app = express();
 const { createToken } = require("./jsonFile");
+const { verify } = require("jsonwebtoken");
+require('dotenv').config()
 
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY
 const PORT = 3000;
 
 app.use(express.json());
 
 const user = {
+  userId: 1,
   firstName: "Victor",
   lastName: "Mugisha",
   username: "VictorMugisha",
   password: "password",
 };
 
+// JWT token validation middleware
+const validateToken = (req, res, next) => {
+    const authHeader = req.headers.authentication;
+    if (!authHeader) {
+        res.status(401).json({
+            success: false,
+            message: 'Token is not provided'
+        });
+    } else {
+        const token = authHeader.split(' ')[1]; // Bearer <token>
+
+        verify(token, JWT_SECRET_KEY, (err, decoded) => {
+            if (err) {
+                return res.status(403).json({
+                    success: false,
+                    message: 'Invalid token'
+                });
+            } else {
+                req.user = decoded;
+                next()
+            }
+        })
+    }
+}
+
 // Mock a login route
 app.post("/login", (req, res) => {
-  const { firstName, lastName, username, password } = req.body;
+  const { username, password } = req.body;
   if (username === user.username && password === user.password) {
+    const token = createToken(user);
+    res.json({ success: true, message: 'Authentication successful!', token: token });
   } else {
-    res.status(400).json({ error: "Invalid username or password" });
+    res
+      .status(401)
+      .json({ success: false, message: "Invalid username or password" });
   }
 });
+
+// now the endpoint for login is http://localhost:3000/login
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
